d45705ebce766854bcd6eef7c823f1e5f2cc7d59
Tara Baskara <tbaskara@starqle.com>
2017-09-06 15:45:10 +0700

    Add temporary repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,25 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repo
+  """
+
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "Tara", username: "tara", password: "tara"},
+      %Rumbl.User{id: "2", name: "Baskara", username: "baskara", password: "baskara"},
+      %Rumbl.User{id: "3", name: "Jenar", username: "jenar", password: "jenar"}
+    ]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
